2a41e55f582fa56a2967e06341d7ae77d8ed9758
Jeff Sparkes <jsparkes@opentext.com>
2024-06-02 06:37:38 -0400

    Add mac compile to make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,8 @@
-g++ NeuralNetwork.cpp main.cpp pngwriter.cc -I../../eigen -I/usr/include/freetype2 -lstdc++ -lpng16 -lfreetype
+case $(uname -s) in
+Darwin)
+	g++ -std=c++17 NeuralNetwork.cpp main.cpp pngwriter.cc -I../../eigen -I/usr/include/freetype2 -I/usr/local/include/freetype2 -lpng16 -lfreetype
+	;;
+*)
+	g++ NeuralNetwork.cpp main.cpp pngwriter.cc -I../../eigen -I/usr/include/freetype2 -lpng16 -lfreetype
+	;;
+esac
